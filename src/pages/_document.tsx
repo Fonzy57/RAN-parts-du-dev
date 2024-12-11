c1a701c12867d5fd9400a6530b0fe13bd97a1a3d
Fonzy57 <traben@hotmail.fr>
2024-12-11 21:53:56 +0100

    Fix browser icon src

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="fr">
       <Head>
-        <link rel="icon" href="/images/logo-browser.png" />
+        <link rel="icon" href="./images/logo-browser.png" />
       </Head>
       <body className="antialiased">
         <Main />
