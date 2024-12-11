3ba817fa7aa96d8869339d4a720f6571640f9d71
Fonzy57 <traben@hotmail.fr>
2024-12-11 18:20:36 +0100

    Add team member info in config

---
+++ src/config/team.ts
@@ -0,0 +1,75 @@
+export interface TeamMembersProps {
+  nom: string;
+  prenom: string;
+  ran: number;
+  linkedin?: string | null;
+}
+
+export const teamMembers: TeamMembersProps[] = [
+  {
+    nom: "BAKHTI",
+    prenom: "Sarah",
+    ran: 2,
+    linkedin: "toto",
+  },
+  {
+    nom: "LALLEMENT",
+    prenom: "Laurent",
+    ran: 4,
+  },
+  {
+    nom: "CHALEKH",
+    prenom: "Mamoud",
+    ran: 3,
+  },
+  {
+    nom: "KUHN",
+    prenom: "Rayan",
+    ran: 3,
+  },
+  {
+    nom: "SCHEERES",
+    prenom: "Stéphane",
+    ran: 4,
+  },
+  {
+    nom: "GARRANAS",
+    prenom: "José",
+    ran: 1,
+  },
+  {
+    nom: "ALABA ONI",
+    prenom: "Edouard",
+    ran: 3,
+  },
+  {
+    nom: "OLIVAREZ",
+    prenom: "Lucas",
+    ran: 4,
+  },
+  {
+    nom: "POLY",
+    prenom: "Lilian",
+    ran: 1,
+  },
+  {
+    nom: "ROUYER",
+    prenom: "Gaetan",
+    ran: 4,
+  },
+  {
+    nom: "JACQUESSON",
+    prenom: "Jim",
+    ran: 1,
+  },
+  {
+    nom: "JASKOWIAK",
+    prenom: "Lucas",
+    ran: 2,
+  },
+  {
+    nom: "BEGNA",
+    prenom: "Mehdi",
+    ran: 1,
+  },
+];
