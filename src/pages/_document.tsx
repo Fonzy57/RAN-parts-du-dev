a00b2e1dab07c847daee4858a416ad92647ad0ee
Fonzy57 <traben@hotmail.fr>
2024-12-11 20:28:07 +0100

    Add browser logo in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
+      <Head>
+        <link rel="icon" href="/images/logo-browser.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
