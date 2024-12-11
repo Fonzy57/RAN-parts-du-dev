c6e8122296ee59fdd9f926a247354d88aadd2250
Fonzy57 <traben@hotmail.fr>
2024-12-11 19:53:02 +0100

    Add formations infos in config file

---
+++ src/config/formations.ts
@@ -0,0 +1,20 @@
+export const formations = [
+  {
+    titre: "Développement Informatique",
+    description:
+      "Découvrez nos formations de niveau BAC à BAC+5 en développement informatique et notamment en développement d’applications. En formation initiale ou en alternance, devenez un acteur de la transformation numérique !",
+    url: "https://www.metz-numeric-school.fr/fr/formations/developpement-informatique",
+  },
+  {
+    titre: "Systèmes, Réseaux et Cybersécurité",
+    description:
+      "Vous souhaitez devenir un spécialiste en Cyber Sécurité, découvrez nos formations de niveau BAC à BAC+5 en formation initiale ou en alternance.",
+    url: "https://www.metz-numeric-school.fr/fr/formations/securite-informatique",
+  },
+  {
+    titre: "Commerce et Marketing Digital",
+    description:
+      "Passionnés d'innovations digitales ? Le Marketing Digital est fait pour vous ! Découvrez l'offre de formation Metz Numeric School.",
+    url: "https://www.metz-numeric-school.fr/fr/formations/commerce-et-marketing-digital",
+  },
+];
