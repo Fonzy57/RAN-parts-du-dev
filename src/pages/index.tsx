807d7cb886d99074d8de3525f952ba9985d6a1bc
Fonzy57 <traben@hotmail.fr>
2024-12-11 18:21:47 +0100

    Change home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
+/* import { Button } from "@/components/button/button"; */
+import Seo from "@/components/seo/seo";
+
 export default function Home() {
   return (
     <>
-      <div>
-        <div>
-          <h1>Accueil</h1>
-        </div>
-      </div>
+      <Seo page="home" />
+      <section className="px-5 pt-10 w-full sm:pt-14 md:p-0 md:pt-20">
+        {/* TODO CHANGER TYPO DU SITE */}
+        <h1 className="text-center text-8xl text-secondary font-montserrat font-bold">
+          RAN-parts du Dev
+        </h1>
+        <p className="lg:text-lg w-[750px] mx-auto mt-8">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+          suscipit animi molestiae voluptas. Saepe corrupti, amet blanditiis
+          soluta, iste atque, quis porro natus distinctio dolor quas laborum.
+          Nemo, ipsam. Adipisci?
+        </p>
+      </section>
     </>
   );
 }
